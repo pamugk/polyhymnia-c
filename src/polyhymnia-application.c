c16e5306d98e12beafb7805fca52da5d637341fe
Alexander Permyakov <pamugk@gmail.com>
2023-09-21 21:19:08 +0500

    UI & App
    - implemented main app window state restoration,
    - implemented simple app configuration

--- src/polyhymnia-application.c
+++ src/polyhymnia-application.c
@@ -7,7 +7,7 @@
 
 struct _PolyhymniaApplication
 {
-	AdwApplication parent_instance;
+  AdwApplication parent_instance;
 };
 
 G_DEFINE_TYPE (PolyhymniaApplication, polyhymnia_application, ADW_TYPE_APPLICATION)
@@ -16,37 +16,37 @@ PolyhymniaApplication *
 polyhymnia_application_new (const char        *application_id,
                             GApplicationFlags  flags)
 {
-	g_return_val_if_fail (application_id != NULL, NULL);
+  g_return_val_if_fail (application_id != NULL, NULL);
 
-	return g_object_new (POLYHYMNIA_TYPE_APPLICATION,
-	                     "application-id", application_id,
-	                     "flags", flags,
-	                     NULL);
+  return g_object_new (POLYHYMNIA_TYPE_APPLICATION,
+	                "application-id", application_id,
+	                "flags", flags,
+	                NULL);
 }
 
 static void
 polyhymnia_application_activate (GApplication *app)
 {
-	GtkWindow *window;
+  GtkWindow *window;
 
-	g_assert (POLYHYMNIA_IS_APPLICATION (app));
+  g_assert (POLYHYMNIA_IS_APPLICATION (app));
 
-	window = gtk_application_get_active_window (GTK_APPLICATION (app));
+  window = gtk_application_get_active_window (GTK_APPLICATION (app));
 
-	if (window == NULL)
-		window = g_object_new (POLYHYMNIA_TYPE_WINDOW,
-		                       "application", app,
-		                       NULL);
+  if (window == NULL)
+    window = g_object_new (POLYHYMNIA_TYPE_WINDOW,
+		            "application", app,
+		            NULL);
 
-	gtk_window_present (window);
+  gtk_window_present (window);
 }
 
 static void
 polyhymnia_application_class_init (PolyhymniaApplicationClass *klass)
 {
-	GApplicationClass *app_class = G_APPLICATION_CLASS (klass);
+  GApplicationClass *app_class = G_APPLICATION_CLASS (klass);
 
-	app_class->activate = polyhymnia_application_activate;
+  app_class->activate = polyhymnia_application_activate;
 }
 
 static void
@@ -54,22 +54,22 @@ polyhymnia_application_about_action (GSimpleAction *action,
                                      GVariant      *parameter,
                                      gpointer       user_data)
 {
-	static const char *developers[] = {"pamugk", NULL};
-	PolyhymniaApplication *self = user_data;
-	GtkWindow *window = NULL;
+  static const char *developers[] = {"pamugk", NULL};
+  PolyhymniaApplication *self = user_data;
+  GtkWindow *window = NULL;
 
-	g_assert (POLYHYMNIA_IS_APPLICATION (self));
+  g_assert (POLYHYMNIA_IS_APPLICATION (self));
 
-	window = gtk_application_get_active_window (GTK_APPLICATION (self));
+  window = gtk_application_get_active_window (GTK_APPLICATION (self));
 
-	adw_show_about_window (window,
-	                       "application-name", "polyhymnia",
-	                       "application-icon", "com.github.pamugk.polyhymnia",
-	                       "developer-name", "pamugk",
-	                       "version", "0.1.0",
-	                       "developers", developers,
-	                       "copyright", "© 2023 pamugk",
-	                       NULL);
+  adw_show_about_window (window,
+	                  "application-name", "Polyhymnia",
+	                  "application-icon", "com.github.pamugk.polyhymnia",
+	                  "developer-name", "pamugk",
+	                  "version", "0.1.0",
+	                  "developers", developers,
+	                  "copyright", "© 2023 pamugk",
+	                  NULL);
 }
 
 static void
@@ -77,21 +77,21 @@ polyhymnia_application_preferences_action (GSimpleAction *action,
                                      GVariant      *parameter,
                                      gpointer       user_data)
 {
-	PolyhymniaApplication *self = user_data;
-	GtkWindow *window = NULL;
-        GtkWindow *preferences_window = NULL;
+  PolyhymniaApplication *self = user_data;
+  GtkWindow *window = NULL;
+  GtkWindow *preferences_window = NULL;
 
-	g_assert (POLYHYMNIA_IS_APPLICATION (self));
+  g_assert (POLYHYMNIA_IS_APPLICATION (self));
 
-	window = gtk_application_get_active_window (GTK_APPLICATION (self));
+  window = gtk_application_get_active_window (GTK_APPLICATION (self));
 
-        preferences_window = g_object_new (POLYHYMNIA_TYPE_PREFERENCES_WINDOW,
-	                                    "application", self,
-		                            NULL);
-        gtk_window_set_modal (preferences_window, TRUE);
-        gtk_window_set_transient_for(preferences_window, window);
+  preferences_window = g_object_new (POLYHYMNIA_TYPE_PREFERENCES_WINDOW,
+	                              "application", self,
+		                      NULL);
+  gtk_window_set_modal (preferences_window, TRUE);
+  gtk_window_set_transient_for(preferences_window, window);
 
-	gtk_window_present (preferences_window);
+  gtk_window_present (preferences_window);
 }
 
 static void
@@ -99,30 +99,30 @@ polyhymnia_application_quit_action (GSimpleAction *action,
                                     GVariant      *parameter,
                                     gpointer       user_data)
 {
-	PolyhymniaApplication *self = user_data;
+  PolyhymniaApplication *self = user_data;
 
-	g_assert (POLYHYMNIA_IS_APPLICATION (self));
+  g_assert (POLYHYMNIA_IS_APPLICATION (self));
 
-	g_application_quit (G_APPLICATION (self));
+  g_application_quit (G_APPLICATION (self));
 }
 
 static const GActionEntry app_actions[] = {
-	{ "about", polyhymnia_application_about_action },
-	{ "quit", polyhymnia_application_quit_action },
-	{ "preferences", polyhymnia_application_preferences_action },
+  { "about", polyhymnia_application_about_action },
+  { "quit", polyhymnia_application_quit_action },
+  { "preferences", polyhymnia_application_preferences_action },
 };
 
 static void
 polyhymnia_application_init (PolyhymniaApplication *self)
 {
-	g_action_map_add_action_entries (G_ACTION_MAP (self),
-	                                 app_actions,
-	                                 G_N_ELEMENTS (app_actions),
-	                                 self);
-	gtk_application_set_accels_for_action (GTK_APPLICATION (self),
-	                                       "app.quit",
-	                                       (const char *[]) { "<primary>q", NULL });
-	gtk_application_set_accels_for_action (GTK_APPLICATION (self),
-	                                       "app.preferences",
-	                                       (const char *[]) { "<primary>comma", NULL });
+  g_action_map_add_action_entries (G_ACTION_MAP (self),
+	                            app_actions,
+	                            G_N_ELEMENTS (app_actions),
+	                            self);
+  gtk_application_set_accels_for_action (GTK_APPLICATION (self),
+	                                  "app.quit",
+	                                  (const char *[]) { "<primary>q", NULL });
+  gtk_application_set_accels_for_action (GTK_APPLICATION (self),
+	                                  "app.preferences",
+	                                  (const char *[]) { "<primary>comma", NULL });
 }
--- src/preferences-window.c
+++ src/preferences-window.c
@@ -5,25 +5,63 @@
 
 struct _PolyhymniaPreferencesWindow
 {
-	AdwPreferencesWindow  parent_instance;
+  AdwPreferencesWindow  parent_instance;
 
-	/* Template widgets */
-	//GtkHeaderBar        *header_bar;
-	//GtkLabel            *label;
+  GSettings *settings;
+
+  /* Template widgets */
+  GtkSwitch        *resume_playback_switch;
+
+  GtkSwitch        *play_explicit_switch;
+  GtkSwitch        *show_explicit_switch;
+  GtkSwitch        *scan_startup_switch;
 };
 
 G_DEFINE_FINAL_TYPE (PolyhymniaPreferencesWindow, polyhymnia_preferences_window, ADW_TYPE_PREFERENCES_WINDOW)
 
 static void
+polyhymnia_preferenes_window_finalize(GObject *gobject)
+{
+  PolyhymniaPreferencesWindow *self = POLYHYMNIA_PREFERENCES_WINDOW (gobject);
+
+  g_clear_object (&self->settings);
+
+  G_OBJECT_CLASS (polyhymnia_preferences_window_parent_class)->finalize (gobject);
+}
+
+static void
 polyhymnia_preferences_window_class_init (PolyhymniaPreferencesWindowClass *klass)
 {
-	GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
 
-	gtk_widget_class_set_template_from_resource (widget_class, "/com/github/pamugk/polyhymnia/ui/preferences-window.ui");
+  gobject_class->finalize = polyhymnia_preferenes_window_finalize;
+
+  gtk_widget_class_set_template_from_resource (widget_class, "/com/github/pamugk/polyhymnia/ui/preferences-window.ui");
+
+  gtk_widget_class_bind_template_child (widget_class, PolyhymniaPreferencesWindow, resume_playback_switch);
+  gtk_widget_class_bind_template_child (widget_class, PolyhymniaPreferencesWindow, play_explicit_switch);
+  gtk_widget_class_bind_template_child (widget_class, PolyhymniaPreferencesWindow, show_explicit_switch);
+  gtk_widget_class_bind_template_child (widget_class, PolyhymniaPreferencesWindow, scan_startup_switch);
 }
 
 static void
 polyhymnia_preferences_window_init (PolyhymniaPreferencesWindow *self)
 {
-	gtk_widget_init_template (GTK_WIDGET (self));
+  gtk_widget_init_template (GTK_WIDGET (self));
+
+  self->settings = g_settings_new ("com.github.pamugk.polyhymnia");
+
+  g_settings_bind (self->settings, "app-system-resume-playback",
+                  self->resume_playback_switch, "active",
+                  G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (self->settings, "app-library-explicit-songs",
+                  self->play_explicit_switch, "active",
+                  G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (self->settings, "app-library-explicit-covers",
+                  self->show_explicit_switch, "active",
+                  G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (self->settings, "app-library-scan-startup",
+                  self->scan_startup_switch, "active",
+                  G_SETTINGS_BIND_DEFAULT);
 }
