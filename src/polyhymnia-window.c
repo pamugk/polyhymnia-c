bab8547b984a02d6aa0a9794b3336bc5de9d14f3
Alexander Permyakov <pamugk@gmail.com>
2023-11-05 01:11:55 +0500

    UI design (libadwaita 1.3 -> 1.4)
    - implemented adaptive sidebar,
    - updated player bar,
    - added basic AdwNavigationView usage

--- src/polyhymnia-window.c
+++ src/polyhymnia-window.c
@@ -8,35 +8,12 @@ struct _PolyhymniaWindow
 {
   AdwApplicationWindow  parent_instance;
 
-  /* Auxiliary objects  */
+  /* Template widgets */
+
+  /* Template objects */
   PolyhymniaMpdClient *mpd_client;
   GSettings           *settings;
 
-  /* Template widgets */
-  GtkButton           *scan_button;
-  GtkButton           *search_button;
-
-  GtkStackSidebar     *library_stack_sidebar;
-  GtkStack            *library_stack;
-  GtkStackPage        *recent_stack_page;
-  GtkStackPage        *artist_stack_page;
-  GtkGridView         *artist_grid_view;
-  GtkStackPage        *album_stack_page;
-  GtkGridView         *album_grid_view;
-  GtkStackPage        *track_stack_page;
-  GtkListView         *track_list_view;
-  GtkStackPage        *genre_stack_page;
-  GtkGridView         *genre_grid_view;
-  AdwStatusPage       *no_library_connection_page;
-
-  GtkActionBar        *player_bar;
-  GtkButton           *previous_track_button;
-  GtkButton           *playback_button;
-  GtkButton           *next_track_button;
-  GtkScale            *track_position_scale;
-  GtkScale            *volume_scale;
-
-  /* Template models */
   GListStore          *album_model;
   GtkNoSelection      *album_selection_model;
   GListStore          *artist_model;
@@ -60,49 +37,17 @@ polyhymnia_window_dispose(GObject *gobject)
 }
 
 static void
-polyhymnia_window_finalize(GObject *gobject)
-{
-  PolyhymniaWindow *self = POLYHYMNIA_WINDOW (gobject);
-
-  g_clear_object (&self->mpd_client);
-  g_clear_object (&self->settings);
-
-  G_OBJECT_CLASS (polyhymnia_window_parent_class)->finalize (gobject);
-}
-
-static void
 polyhymnia_window_class_init (PolyhymniaWindowClass *klass)
 {
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
 
   gobject_class->dispose = polyhymnia_window_dispose;
-  gobject_class->finalize = polyhymnia_window_finalize;
 
   gtk_widget_class_set_template_from_resource (widget_class, "/com/github/pamugk/polyhymnia/ui/polyhymnia-window.ui");
 
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, scan_button);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, search_button);
-
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, library_stack_sidebar);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, library_stack);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, recent_stack_page);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, artist_stack_page);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, artist_grid_view);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, album_stack_page);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, album_grid_view);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, track_stack_page);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, track_list_view);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, genre_stack_page);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, genre_grid_view);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, no_library_connection_page);
-
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, player_bar);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, previous_track_button);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, playback_button);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, next_track_button);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, track_position_scale);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, volume_scale);
+  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, mpd_client);
+  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, settings);
 
   gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, album_selection_model);
   gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, artist_selection_model);
@@ -206,18 +151,6 @@ polyhymnia_window_init (PolyhymniaWindow *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
 
-  self->mpd_client = g_object_new (POLYHYMNIA_TYPE_MPD_CLIENT, NULL);
-  g_object_bind_property(self->mpd_client, "initialized",
-                         self->scan_button, "visible",
-                         G_BINDING_DEFAULT | G_BINDING_SYNC_CREATE);
-  g_object_bind_property(self->mpd_client, "initialized",
-                         self->search_button, "visible",
-                         G_BINDING_DEFAULT | G_BINDING_SYNC_CREATE);
-
-  g_object_bind_property(self->mpd_client, "initialized",
-                         self->player_bar, "visible",
-                         G_BINDING_DEFAULT | G_BINDING_SYNC_CREATE);
-
   self->album_model = g_list_store_new (POLYHYMNIA_TYPE_ALBUM);
   gtk_no_selection_set_model (self->album_selection_model,
                               G_LIST_MODEL (self->album_model));
@@ -227,8 +160,6 @@ polyhymnia_window_init (PolyhymniaWindow *self)
   self->track_model = g_list_store_new (POLYHYMNIA_TYPE_TRACK);
   gtk_multi_selection_set_model (self->track_selection_model,
                                  G_LIST_MODEL (self->track_model));
-
-  self->settings = g_settings_new ("com.github.pamugk.polyhymnia");
 
   g_settings_bind (self->settings, "window-width",
                     self, "default-width",
