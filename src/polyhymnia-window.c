2cf3866cc912e73941e690075cae8adf16104c6f
Alexander Permyakov <pamugk@gmail.com>
2023-11-09 00:22:09 +0500

    MPD-based player
    - implemented window content refresh on DB update,
    - implemented queue pane refresh on queue update

--- src/polyhymnia-window.c
+++ src/polyhymnia-window.c
@@ -6,6 +6,7 @@
 
 #define _(x) g_dgettext (GETTEXT_PACKAGE, x)
 
+/* Type metadata */
 struct _PolyhymniaWindow
 {
   AdwApplicationWindow  parent_instance;
@@ -13,7 +14,7 @@ struct _PolyhymniaWindow
   /* Template widgets */
   AdwOverlaySplitView *content;
   AdwStatusPage       *no_mpd_connection_page;
-  AdwToolbarView      *root_toolbar_view;
+  AdwToastOverlay     *root_toast_overlay;
 
   AdwBin              *artist_stack_page_content;
   AdwNavigationView   *artist_navigation_view;
@@ -31,6 +32,10 @@ struct _PolyhymniaWindow
   AdwNavigationView   *genre_navigation_view;
   AdwStatusPage       *genres_status_page;
 
+  GtkScrolledWindow   *queue_page_content;
+  GtkListView         *queue_list_view;
+  AdwStatusPage       *queue_status_page;
+
   /* Template objects */
   PolyhymniaMpdClient *mpd_client;
   GSettings           *settings;
@@ -43,10 +48,35 @@ struct _PolyhymniaWindow
   GtkNoSelection      *genre_selection_model;
   GListStore          *track_model;
   GtkMultiSelection   *track_selection_model;
+
+  GListStore          *queue_model;
+  GtkNoSelection      *queue_selection_model;
 };
 
 G_DEFINE_FINAL_TYPE (PolyhymniaWindow, polyhymnia_window, ADW_TYPE_APPLICATION_WINDOW)
 
+/* Event handler & other utility functions declaration */
+static void
+polyhymnia_window_content_clear (PolyhymniaWindow *self);
+
+static void
+polyhymnia_window_content_init (PolyhymniaWindow *self);
+
+static void
+polyhymnia_window_mpd_client_initialized(GObject* self,
+                                         GParamSpec* pspec,
+                                         gpointer user_data);
+
+static void
+polyhymnia_window_mpd_database_updated(GObject* self, gpointer user_data);
+
+static void
+polyhymnia_window_mpd_queue_modified(GObject* self, gpointer user_data);
+
+static void
+polyhymnia_window_queue_pane_init (PolyhymniaWindow *self);
+
+/* Class stuff */
 static void
 polyhymnia_window_dispose(GObject *gobject)
 {
@@ -69,7 +99,7 @@ polyhymnia_window_class_init (PolyhymniaWindowClass *klass)
 
   gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, content);
   gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, no_mpd_connection_page);
-  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, root_toolbar_view);
+  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, root_toast_overlay);
 
   gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, artist_stack_page_content);
   gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, artist_navigation_view);
@@ -84,6 +114,10 @@ polyhymnia_window_class_init (PolyhymniaWindowClass *klass)
   gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, track_navigation_view);
   gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, tracks_status_page);
 
+  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, queue_page_content);
+  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, queue_list_view);
+  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, queue_status_page);
+
   gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, mpd_client);
   gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, settings);
 
@@ -92,6 +126,71 @@ polyhymnia_window_class_init (PolyhymniaWindowClass *klass)
   gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, genre_model);
   gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, genre_selection_model);
   gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, track_selection_model);
+
+  gtk_widget_class_bind_template_child (widget_class, PolyhymniaWindow, queue_selection_model);
+}
+
+static void
+polyhymnia_window_init (PolyhymniaWindow *self)
+{
+  gtk_widget_init_template (GTK_WIDGET (self));
+
+  self->album_model = g_list_store_new (POLYHYMNIA_TYPE_ALBUM);
+  gtk_no_selection_set_model (self->album_selection_model,
+                              G_LIST_MODEL (self->album_model));
+  self->artist_model = g_list_store_new (POLYHYMNIA_TYPE_ARTIST);
+  gtk_no_selection_set_model (self->artist_selection_model,
+                              G_LIST_MODEL (self->artist_model));
+  self->track_model = g_list_store_new (POLYHYMNIA_TYPE_TRACK);
+  gtk_multi_selection_set_model (self->track_selection_model,
+                                 G_LIST_MODEL (self->track_model));
+
+  self->queue_model = g_list_store_new (POLYHYMNIA_TYPE_TRACK);
+  gtk_no_selection_set_model (self->queue_selection_model,
+                              G_LIST_MODEL (self->queue_model));
+
+  g_settings_bind (self->settings, "window-width",
+                    self, "default-width",
+                    G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (self->settings, "window-height",
+                    self, "default-height",
+                    G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (self->settings, "window-maximized",
+                    self, "maximized",
+                    G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (self->settings, "window-fullscreened",
+                    self, "fullscreened",
+                    G_SETTINGS_BIND_DEFAULT);
+
+  polyhymnia_window_mpd_client_initialized (G_OBJECT(self->mpd_client), NULL, self);
+  g_signal_connect (self->mpd_client, "notify::initialized",
+                    G_CALLBACK (polyhymnia_window_mpd_client_initialized),
+                    self);
+
+  g_signal_connect (self->mpd_client, "database-updated",
+                    G_CALLBACK (polyhymnia_window_mpd_database_updated),
+                    self);
+  g_signal_connect (self->mpd_client, "queue-modified",
+                    G_CALLBACK (polyhymnia_window_mpd_queue_modified),
+                    self);
+}
+
+/* Event handler & other utility functions implementation */
+static void
+polyhymnia_window_content_clear (PolyhymniaWindow *self)
+{
+  GListModel *genres_list = G_LIST_MODEL (self->genre_model);
+  guint genres_count = g_list_model_get_n_items (genres_list);
+
+  g_list_store_remove_all (self->artist_model);
+  g_list_store_remove_all (self->album_model);
+  g_list_store_remove_all (self->track_model);
+
+  while (genres_count > 0)
+  {
+    gtk_string_list_remove (self->genre_model, genres_count - 1);
+    genres_count = g_list_model_get_n_items (genres_list);
+  }
 }
 
 static void
@@ -254,7 +353,7 @@ polyhymnia_window_content_init (PolyhymniaWindow *self)
 }
 
 static void
-polyhymnia_window_mpd_initialized(GObject* self,
+polyhymnia_window_mpd_client_initialized(GObject* self,
                                   GParamSpec* pspec,
                                   gpointer user_data)
 {
@@ -266,59 +365,82 @@ polyhymnia_window_mpd_initialized(GObject* self,
   g_object_get (window_self->mpd_client, "initialized", &mpd_initialized, NULL);
   if (mpd_initialized)
   {
-    adw_toolbar_view_set_content (window_self->root_toolbar_view,
-                                  GTK_WIDGET (window_self->content));
+    adw_toast_overlay_set_child (window_self->root_toast_overlay,
+                                 GTK_WIDGET (window_self->content));
     polyhymnia_window_content_init (window_self);
+    polyhymnia_window_queue_pane_init (window_self);
   }
   else
   {
-    GListModel *genres_list = G_LIST_MODEL (window_self->genre_model);
-    guint genres_count = g_list_model_get_n_items (genres_list);
-
-    adw_toolbar_view_set_content (window_self->root_toolbar_view,
-                                  GTK_WIDGET (window_self->no_mpd_connection_page));
-    g_list_store_remove_all (window_self->artist_model);
-    g_list_store_remove_all (window_self->album_model);
-    g_list_store_remove_all (window_self->track_model);
-
-    while (genres_count > 0)
-    {
-      gtk_string_list_remove (window_self->genre_model, genres_count - 1);
-      genres_count = g_list_model_get_n_items (genres_list);
-    }
+    adw_toast_overlay_set_child (window_self->root_toast_overlay,
+                                 GTK_WIDGET (window_self->no_mpd_connection_page));
+    g_list_store_remove_all (window_self->queue_model);
+    polyhymnia_window_content_clear (window_self);
   }
 }
 
 static void
-polyhymnia_window_init (PolyhymniaWindow *self)
+polyhymnia_window_mpd_database_updated(GObject* self, gpointer user_data)
 {
-  gtk_widget_init_template (GTK_WIDGET (self));
+  AdwToast *database_updated_toast = adw_toast_new (_("Library has been updated"));
+  PolyhymniaWindow *window_self = user_data;
 
-  self->album_model = g_list_store_new (POLYHYMNIA_TYPE_ALBUM);
-  gtk_no_selection_set_model (self->album_selection_model,
-                              G_LIST_MODEL (self->album_model));
-  self->artist_model = g_list_store_new (POLYHYMNIA_TYPE_ARTIST);
-  gtk_no_selection_set_model (self->artist_selection_model,
-                              G_LIST_MODEL (self->artist_model));
-  self->track_model = g_list_store_new (POLYHYMNIA_TYPE_TRACK);
-  gtk_multi_selection_set_model (self->track_selection_model,
-                                 G_LIST_MODEL (self->track_model));
+  g_assert (POLYHYMNIA_IS_WINDOW (window_self));
 
-  g_settings_bind (self->settings, "window-width",
-                    self, "default-width",
-                    G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (self->settings, "window-height",
-                    self, "default-height",
-                    G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (self->settings, "window-maximized",
-                    self, "maximized",
-                    G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (self->settings, "window-fullscreened",
-                    self, "fullscreened",
-                    G_SETTINGS_BIND_DEFAULT);
+  adw_toast_overlay_add_toast (window_self->root_toast_overlay,
+                               database_updated_toast);
+  polyhymnia_window_content_clear (window_self);
+  polyhymnia_window_content_init (window_self);
+}
 
-  polyhymnia_window_mpd_initialized (G_OBJECT(self->mpd_client), NULL, self);
-  g_signal_connect (self->mpd_client, "notify::initialized",
-                    G_CALLBACK (polyhymnia_window_mpd_initialized),
-                    self);
+static void
+polyhymnia_window_mpd_queue_modified(GObject* self, gpointer user_data)
+{
+  PolyhymniaWindow *window_self = user_data;
+
+  g_assert (POLYHYMNIA_IS_WINDOW (window_self));
+
+  g_list_store_remove_all (window_self->queue_model);
+  polyhymnia_window_queue_pane_init (window_self);
+}
+
+static void
+polyhymnia_window_queue_pane_init (PolyhymniaWindow *self)
+{
+  GError *error = NULL;
+  GPtrArray *queue = polyhymnia_mpd_client_get_queue (self->mpd_client,
+                                                      &error);
+  if (error != NULL)
+  {
+    g_object_set (G_OBJECT (self->queue_status_page),
+                  "description", NULL,
+                  "icon-name", "error-symbolic",
+                  "title", _("Queue fetch failed"),
+                  NULL);
+    gtk_scrolled_window_set_child (self->queue_page_content,
+                                   GTK_WIDGET (self->queue_status_page));
+    g_warning("Queue fetch failed: %s\n", error->message);
+    g_error_free (error);
+    error = NULL;
+  }
+  else if (queue->len == 0)
+  {
+    g_ptr_array_free (queue, FALSE);
+    g_object_set (G_OBJECT (self->queue_status_page),
+                  "description", _("Queue is empty"),
+                  NULL);
+    gtk_scrolled_window_set_child (self->queue_page_content,
+                                   GTK_WIDGET (self->queue_status_page));
+  }
+  else
+  {
+    for (int i = 0; i < queue->len; i++)
+    {
+      PolyhymniaTrack *queue_track = g_ptr_array_index(queue, i);
+      g_list_store_append (self->queue_model, queue_track);
+    }
+    g_ptr_array_free (queue, TRUE);
+    gtk_scrolled_window_set_child (self->queue_page_content,
+                                   GTK_WIDGET (self->genre_navigation_view));
+  }
 }
