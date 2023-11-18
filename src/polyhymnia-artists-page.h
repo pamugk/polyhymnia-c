08d5ea863a5f2c2edf688e044f9b800eb0424aed
Alexander Permyakov <pamugk@gmail.com>
2023-11-18 20:04:57 +0500

    Navigation
    - split out artists page into a separate component,
    - added some improvements for an artists split view

---
+++ src/polyhymnia-artists-page.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include <adwaita.h>
+
+G_BEGIN_DECLS
+
+#define POLYHYMNIA_TYPE_ARTISTS_PAGE (polyhymnia_artists_page_get_type())
+
+G_DECLARE_FINAL_TYPE (PolyhymniaArtistsPage, polyhymnia_artists_page, POLYHYMNIA, ARTISTS_PAGE, AdwNavigationPage)
+
+G_END_DECLS
